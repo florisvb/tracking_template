37f5fc9dd68a5cad4c2d491c3bc688d1a0c23672
Floris van Breugel <florisvb@gmail.com>
2016-04-12 16:34:48 -0700

    added some more clues

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -21,11 +21,20 @@ data.extend(p_vector)
 Thus...        
 '''
 
-time = data['data_0']
+time = data['data_0'] 
+# note: the time is represented in "time since the epoch", hence it starts at a very large number
+# if you would like time to start at 0, you can subtract, the first, or the minimal, value of time from time
 x = data['data_1']
 xdot = data['data_2']
 y = data['data_3']
 ydot = data['data_4']
+
+# extract the position and velocity covariances from the data 
+# these values represent the diagonal of the 4x4 covariance matrix 
+rxx = data['data_5']
+rxdotxdot = data['data_10']
+ryy = data['data_15']
+rydotydot = data['data_20']
 
 # and so on
 
@@ -35,3 +44,8 @@ ax1.plot(time, x)
 
 ax2 = fig.add_subplot(122)
 ax2.plot(x, y)
+
+# some other handy commands:
+
+ax2.set_xlim(0,1) # set the limits on the x axis, same goes for the y axis
+fig.savefig('filename.png', format='png') # save a figure as a png 
